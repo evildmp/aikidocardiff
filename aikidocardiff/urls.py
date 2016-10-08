ea02672508f89a19f3a9fd788c22b77fbf41968c
evildmp <daniele@vurt.org>
2016-10-08 22:11:35 +0100

    Update to match s7 version

--- aikidocardiff/urls.py
+++ aikidocardiff/urls.py
@@ -1,27 +1,28 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^test_project/', include('test_project.foo.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    ('^admin/', include(admin.site.urls)),
- 
+    url(r'^admin/', include(admin.site.urls)),
+
     (r'^semantic/', include('semanticeditor.urls')),
     (r"", include("contacts_and_people.urls")),
+    url(r"", include("links.urls")),
 
     (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
 )
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += patterns('',
     url('^autocomplete/$', 'widgetry.views.search', name='widgetry-search'),
     url(r'^', include('cms.urls')),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
