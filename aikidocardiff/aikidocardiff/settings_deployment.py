a57337025160c313ccf4455cc26d902358fca0cc
Daniele Procida <daniele@apple-juice.co.uk>
2017-09-02 22:02:08 +0200

    Added all the files

---
+++ aikidocardiff/aikidocardiff/settings_deployment.py
@@ -0,0 +1,9 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+FORCE_SCRIPT_NAME = ''
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'aikidocardiff.wsgi.application'
